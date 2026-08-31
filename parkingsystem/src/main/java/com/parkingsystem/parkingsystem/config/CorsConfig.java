baf14272e9723dae166263c4d327df0190fc27be
Jeel <shahjeel459@gmail.com>
2026-08-31 23:08:59 +0530

    prepare backend for production

--- parkingsystem/src/main/java/com/parkingsystem/parkingsystem/config/CorsConfig.java
+++ parkingsystem/src/main/java/com/parkingsystem/parkingsystem/config/CorsConfig.java
@@ -1,5 +1,6 @@
-package com.parkingsystem.parkingsystem.filter;
+package com.parkingsystem.parkingsystem.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,10 +12,14 @@ import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${eazystore.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
         corsConfiguration.setAllowCredentials(true);
